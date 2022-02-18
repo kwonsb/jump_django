d0b516bad540ce390e979bf6ffca7056df7b1f40
ksb <ksoonb85@gmail.com>
2022-02-18 14:55:38 +0900

    prod setting

---
+++ config/settings/prod.py
@@ -0,0 +1,4 @@
+from ssl import AlertDescription
+from .base import *
+
+ALLOWED_HOSTS = ['34.237.35.42']
---
+++ prod-init.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+source venv/bin/activate
+cd /home/ubuntu/jump_django
+export DJANGO_SETTINGS_MODULE=config.settings.prod
